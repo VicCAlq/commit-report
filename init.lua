20dd32829919cc3cb34ecfae46d8f70021994a27
Victor Cavalcanti <victor.mca.dev@gmail.com>
2024-10-01 17:23:44 -0300

    p.feature: Testing the 'lapp' module

--- init.lua
+++ init.lua
@@ -1,45 +1,14 @@
-local pl = require("pl.import_into")()
-local path = "/home/vicmca/CodeProjects/CESAR/Petro/t12/estimador_de_sobressalentes/frontend/"
+local Date = require("pl.Date")
+local lapp = require("pl.lapp")
+local path = require("pl.path")
 local parser = require("src.parser")
 local filter = require("src.filter")
 
-local all_branches = io.popen(string.format("cd %s && git branch -a", path), "r")
+local args = lapp([[
+  Test for lapp
+    -l, --layout (default "v")  Layout orientation ("v" for Vertical, "h" for Horizontal)
+    -d, --days (number, number)  Range of days
+    <url> (string)  URL for the GIT repo
+]])
 
-local commits = {}
-
-for k, v in pairs(pl.Date) do
-  print(k, v)
-end
-
-if all_branches ~= nil then
-  local remote_branches, local_branches = parser.categorize_branches(parser.branches_to_table(all_branches))
-  filter.remove_inactive_branches(local_branches)
-
-  for branch in all_branches:lines() do
-    if string.find(branch, "remotes/origin") then
-      print("skipped")
-    else
-      local commit_info = {}
-      local commit =
-        io.popen(string.format("cd %s && " .. "git stash && " .. "git checkout %s && " .. "git log -1", path, branch))
-
-      if commit ~= nil then
-        for commit_line in commit:lines() do
-          if string.find(commit_line, "Author") then
-            commit_info.author = commit_line
-          elseif string.find(commit_line, "Date") then
-            commit_info.date = commit_line
-          end
-        end
-      end
-
-      table.insert(commits, commit_info)
-    end
-  end
-end
-
-for k, t in pairs(commits) do
-  for _, v in pairs(t) do
-    -- print(v)
-  end
-end
+print(args.layout, args.days, args.url)
