86821aa6661004a5973e5aec44cdbfd9345e04ae
Victor Cavalcanti <victor.mca.dev@gmail.com>
2024-10-02 13:03:04 -0300

    p.test: Added a sample SQL file

--- table_test.sql
+++ table_test.sql
@@ -1,21 +1,39 @@
 -- ["remotes/origin/main"] = {
 --    {
---      author_email = "alexmavr@users.noreply.github.com",
---      author_name = "Alex Mavrogiannis",
---      branch = "main",
---      commit = "f40bb398f6423ae82386820f0abfe279045771b7",
---      date = "Tue Oct 1 15:45:43 2024 -0700",
---      description = "Stop model before deletion if loaded (fixed #6957) (#7050)",
---      time = 1727808343
+--      author_email = "mxyng@pm.me",
+--      author_name = "Michael Yang",
+--      branch = "mxyng/mllama",
+--      commit = "3e8d03f8f6b723f5d3df90ab4db0e6d6983029f4",
+--      date = "2024-09-30 11:13:49.000",
+--      description = "integrate mllama.cpp to server.cpp",
+--      time = 1727705629
 --    },
 -- }
 
-CREATE TABLE test_repo(
-  commit VARCHAR(50) NOT NULL UNIQUE PRIMARY KEY,
-  time INT NOT NULL,
-  author_name VARCHAR(200) NOT NULL,
-  author_email VARCHAR(200) NOT NULL,
-  branch VARCHAR(200) NOT NULL,
-  date TEXT NOT NULL,
-  description TEXT NOT NULL,
-)
+CREATE TABLE IF NOT EXISTS test_repo (
+commit_hash VARCHAR(50) NOT NULL UNIQUE PRIMARY KEY,
+unix_time INT NOT NULL,
+author_name VARCHAR(200) NOT NULL,
+author_email VARCHAR(200) NOT NULL,
+branch VARCHAR(200) NOT NULL,
+date TEXT NOT NULL,
+description TEXT NOT NULL
+);
+
+INSERT INTO test_repo (
+commit_hash, 
+unix_time, 
+author_name, 
+author_email, 
+branch, 
+date, 
+description
+) VALUES (
+'3e8d03f8f6b723f5d3df90ab4db0e6d6983029f4',
+1727705629,
+'Michael Yang',
+'mxyng@pm.me',
+'mxyng/mllama',
+'2024-09-30 11:13:49.000',
+'integrate mllama.cpp to server.cpp'
+);
